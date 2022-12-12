33f303625faf5715253c6b9e1994f4ed75aa8288
SHI ZHEN <shi.zhen@vincit.fi>
2022-12-12 21:02:47 +0200

    add connect error handler

--- connection.js
+++ connection.js
@@ -1,9 +1,10 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
+const mongoose = require('mongoose')
+require('dotenv').config()
 
 mongoose.connect(
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@cluster0.7xg2i.mongodb.net/chatAppMern?retryWrites=true&w=majority`,
-  () => {
-    console.log("connect to mongodb");
-  }
-);
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@cluster0.7xg2i.mongodb.net/chatAppMern?retryWrites=true&w=majority`
+).then(() => {
+  console.log('connect to mongodb')
+}).catch((e) => {
+  console.log(e)
+})
